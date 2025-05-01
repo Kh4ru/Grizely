cf0bcac748225973b158f303ad429102ff83e182
Senepart Alexis <89015385+Kh4ru@users.noreply.github.com>
2025-05-01 21:11:44 +0000

    First version (0.1)

--- grizely.js
+++ grizely.js
@@ -1,16 +1,22 @@
+let grizely_version = "0.1"
+let grizely_credits = "Developped by Alexis Senepart"
 class Page{
-  constructor(title = "Page"){
+  constructor(title = "Page",style = {}){
+    this.style = style
     this.title = title
     document.title = this.title
+    document.body.style.backgroundColor = style["background_color"]
   }
 }
 class Styledoc{
-  constructor(font_color = "black",font_size="50px"){
+  constructor(font_color = "black",font_size="",backgroundColor = ""){
     this.font_color = font_color;
     this.font_size = font_size;
+    this.backgroundColor = backgroundColor;
     this.data = {
       "font_color":this.font_color,
-      "font_size":this.font_size
+      "font_size":this.font_size,
+      "background_color":this.backgroundColor
     }
     return this.data
   }
@@ -25,8 +31,7 @@ class Titre{
     node.innerHTML = text
     node.style.color = style["font_color"]
     node.style.fontSize = style["font_size"]
+    node.style.backgroundColor = style["background_color"]
     document.body.appendChild(node)
   }
 }
-let style = new Styledoc("black","50px")
-let mon_titre = new Titre("Comment ça va ?",style)
--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 let page = new Page("YO")
+let style = new Styledoc("red")
+let titre = new Titre("test",style)
